27e2ff525367f00457087d72652de25924e5a2b6
Brandon <bkw1491@gmail.com>
2023-08-30 10:05:15 +0100

    add interactive login endpoint

--- src/BetfairDotNet/Endpoints/LoginEndpoints.cs
+++ src/BetfairDotNet/Endpoints/LoginEndpoints.cs
@@ -2,5 +2,6 @@
 internal static class LoginEndpoints {
 
     internal const string CertificateLogin = "https://identitysso-cert.betfair.com/api/certlogin";
+    internal const string InteractiveLogin = "https://identitysso.betfair.com/api/login";
     internal const string Logout = "https://identitysso.betfair.com/api/logout";
 }

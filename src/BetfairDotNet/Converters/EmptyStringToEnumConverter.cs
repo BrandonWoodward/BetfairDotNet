4b2cbba436000ab441633d3bf1864057443840ee
Brandon <bkw1491@gmail.com>
2023-08-30 10:05:05 +0100

    Add converter to serialzie empty string to default enum

---
+++ src/BetfairDotNet/Converters/EmptyStringToEnumConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BetfairDotNet.Converters;
+
+
+internal class EmptyStringToEnumConverter<TEnum> : JsonConverter<TEnum> where TEnum : Enum {
+
+    public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        var str = reader.GetString();
+        if(string.IsNullOrEmpty(str)) {
+            return (TEnum)Enum.GetValues(typeToConvert).GetValue(0)!;
+        }
+        if(Enum.TryParse(typeToConvert, str, true, out var result)) {
+            return (TEnum)result;
+        }
+        return (TEnum)Enum.GetValues(typeToConvert).GetValue(0)!;
+    }
+
+
+    public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options) {
+        writer.WriteStringValue(value.ToString());
+    }
+}

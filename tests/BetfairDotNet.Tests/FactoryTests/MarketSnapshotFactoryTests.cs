637e20f04d9c0fb50a5a3e934789d445e420faa0
Brandon <bkw1491@gmail.com>
2023-08-31 18:58:55 +0100

    Add test case for heartbeat

---
+++ tests/BetfairDotNet.Tests/FactoryTests/MarketSnapshotFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using BetfairDotNet.Enums.Streaming;
+using BetfairDotNet.Factories;
+using BetfairDotNet.Models.Streaming;
+using FluentAssertions;
+using System.Collections.Concurrent;
+using Xunit;
+
+namespace BetfairDotNet.Tests.FactoryTests;
+
+
+public class MarketSnapshotFactoryTests {
+
+
+    [Fact]
+    public void ProcessImage_ShouldReturnEmpty_WhenMessageIsHeartbeat() {
+        // Arrange
+        var changeMessage = new MarketChangeMessage() { Id = 123, ChangeType = ChangeTypeEnum.HEARTBEAT };
+        var cache = new ConcurrentDictionary<string, MarketSnapshot>();
+
+        // Act
+        var sut = new MarketSnapshotFactory(cache);
+        var actual = sut.GetSnapshots(changeMessage);
+
+        // Assert
+        actual.Should().BeEmpty();
+    }
+}

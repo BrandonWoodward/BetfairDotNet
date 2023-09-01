8e14c7171e120c7953e14488b24834aa0a8f76ee
Brandon <bkw1491@gmail.com>
2023-09-01 15:14:22 +0100

    add missing test case

--- tests/BetfairDotNet.Tests/ConverterTests/DoubleNaNToNullConverterTests.cs
+++ tests/BetfairDotNet.Tests/ConverterTests/DoubleNaNToNullConverterTests.cs
@@ -17,6 +17,18 @@ public class DoubleNaNToNullConverterTests {
     }
 
 
+    private string WriteToJson(double? value) {
+        var stream = new MemoryStream();
+        var writer = new Utf8JsonWriter(stream);
+        var converter = new DoubleNaNToNullConverter();
+
+        converter.Write(writer, value, new JsonSerializerOptions());
+
+        writer.Flush();
+        return Encoding.UTF8.GetString(stream.ToArray());
+    }
+
+
     [Fact]
     public void Read_Should_Return_Null_For_NaN() {
         var value = ReadFromJson("\"NaN\"");
@@ -47,5 +59,26 @@ public class DoubleNaNToNullConverterTests {
     [Fact]
     public void Read_Should_Throw_Exception_For_Unexpected_Token() {
         Assert.Throws<JsonException>(() => ReadFromJson("{ }"));
+    }
+
+
+    [Fact]
+    public void Read_Should_Return_Number_For_Numeric_Token() {
+        var value = ReadFromJson("42.42");
+        Assert.Equal(42.42, value);
+    }
+
+
+    [Fact]
+    public void Write_Should_Write_Null_For_Null_Value() {
+        var json = WriteToJson(null);
+        Assert.Equal("null", json);
+    }
+
+
+    [Fact]
+    public void Write_Should_Write_Number_For_Number_Value() {
+        var json = WriteToJson(42.42);
+        Assert.Equal("42.42", json);
     }
 }

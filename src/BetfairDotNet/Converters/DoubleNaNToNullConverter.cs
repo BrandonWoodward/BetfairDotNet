939f79f8c33a3252878ba1f6e9958a2293cb08e7
Brandon <bkw1491@gmail.com>
2023-09-01 11:45:14 +0100

    add test cases for DoubleNaNToNullConverter

--- src/BetfairDotNet/Converters/DoubleNaNToNullConverter.cs
+++ src/BetfairDotNet/Converters/DoubleNaNToNullConverter.cs
@@ -4,14 +4,10 @@ using System.Text.Json.Serialization;
 namespace BetfairDotNet.Converters;
 
 
-internal class DoubleNaNToNullConverter : JsonConverter<double?>
-{
+internal class DoubleNaNToNullConverter : JsonConverter<double?> {
 
-
-    public override double? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        if (reader.TokenType == JsonTokenType.String)
-        {
+    public override double? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        if(reader.TokenType == JsonTokenType.String) {
             var stringValue = reader.GetString();
             return stringValue == "NaN"
                 ? null
@@ -19,21 +15,18 @@ internal class DoubleNaNToNullConverter : JsonConverter<double?>
                 ? value
                 : throw new JsonException($"Unexpected value {stringValue} for double.");
         }
-        else if (reader.TokenType == JsonTokenType.Number)
-        {
+        else if(reader.TokenType == JsonTokenType.Number) {
             return reader.GetDouble();
         }
-        else if (reader.TokenType == JsonTokenType.Null)
-        {
+        else if(reader.TokenType == JsonTokenType.Null) {
             return null;
         }
         throw new JsonException($"Unexpected token type {reader.TokenType}.");
     }
 
 
-    public override void Write(Utf8JsonWriter writer, double? value, JsonSerializerOptions options)
-    {
-        if (value.HasValue)
+    public override void Write(Utf8JsonWriter writer, double? value, JsonSerializerOptions options) {
+        if(value.HasValue)
             writer.WriteNumberValue(value.Value);
         else
             writer.WriteNullValue();
---
+++ tests/BetfairDotNet.Tests/ConverterTests/DoubleNaNToNullConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using BetfairDotNet.Converters;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace BetfairDotNet.Tests.ConverterTests;
+
+
+public class DoubleNaNToNullConverterTests {
+
+
+    private double? ReadFromJson(string json) {
+        var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json), isFinalBlock: true, state: default);
+        reader.Read();
+        var converter = new DoubleNaNToNullConverter();
+        return converter.Read(ref reader, typeof(double?), new JsonSerializerOptions());
+    }
+
+
+    [Fact]
+    public void Read_Should_Return_Null_For_NaN() {
+        var value = ReadFromJson("\"NaN\"");
+        Assert.Null(value);
+    }
+
+
+    [Fact]
+    public void Read_Should_Return_Number_For_Numeric_String() {
+        var value = ReadFromJson("\"42.42\"");
+        Assert.Equal(42.42, value);
+    }
+
+
+    [Fact]
+    public void Read_Should_Return_Null_For_Null_Token() {
+        var value = ReadFromJson("null");
+        Assert.Null(value);
+    }
+
+
+    [Fact]
+    public void Read_Should_Throw_Exception_For_Invalid_String() {
+        Assert.Throws<JsonException>(() => ReadFromJson("\"invalid\""));
+    }
+
+
+    [Fact]
+    public void Read_Should_Throw_Exception_For_Unexpected_Token() {
+        Assert.Throws<JsonException>(() => ReadFromJson("{ }"));
+    }
+}

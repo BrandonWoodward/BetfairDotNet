7a8b3b1b34aee904ff41ae114c9026844b7aeaad
Brandon <bkw1491@gmail.com>
2023-09-01 18:51:37 +0100

    add missing test case for interactive login model

---
+++ tests/BetfairDotNet.Tests/ModelsTests/Login/InteractiveLoginResponseTests.cs
@@ -0,0 +1,26 @@
+﻿using BetfairDotNet.Converters;
+using BetfairDotNet.Enums.Account;
+using BetfairDotNet.Models.Login;
+using FluentAssertions;
+using Xunit;
+
+namespace BetfairDotNet.Tests.ModelsTests.Login;
+
+public class InteractiveLoginResponseTests {
+
+    [Fact]
+    public void InteractiveLoginResponse_ShouldDeserializeCorrectly() {
+        // Arrange
+        var interactiveLoginResponse = new InteractiveLoginResponse() {
+            SessionToken = "sessionToken",
+            Status = LoginStatusEnum.ACTIONS_REQUIRED,
+        };
+
+        // Act
+        var json = JsonConvert.Serialize(interactiveLoginResponse);
+        var deserialized = JsonConvert.Deserialize<InteractiveLoginResponse>(json);
+
+        // Assert
+        deserialized.Should().BeEquivalentTo(interactiveLoginResponse);
+    }
+}

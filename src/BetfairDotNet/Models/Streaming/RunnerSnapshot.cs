a505172db600776af63d70a3e36f99accbf21d31
Brandon <bkw1491@gmail.com>
2023-09-01 09:40:11 +0100

    add test cases for image and delta

--- src/BetfairDotNet/Models/Streaming/RunnerSnapshot.cs
+++ src/BetfairDotNet/Models/Streaming/RunnerSnapshot.cs
@@ -9,40 +9,40 @@ public sealed record RunnerSnapshot {
     /// <summary>
     /// The unique identifier for the selection.
     /// </summary>
-    public required long SelectionId { get; init; }
+    public long SelectionId { get; init; }
 
     /// <summary>
     /// The selection's metadata if requested.
     /// </summary>
-    public required RunnerDefinition? RunnerDefinition { get; init; }
+    public RunnerDefinition? RunnerDefinition { get; init; }
 
     /// <summary>
     /// Amounts available to lay by distinct price. Filtered as per <see cref="MarketDataFilter"/>
     /// </summary>
-    public required PriceLadder ToLay { get; init; }
+    public PriceLadder ToLay { get; init; }
 
     /// <summary>
     /// Amounts available to back by distinct price. Filtered as per <see cref="MarketDataFilter"/>
     /// </summary>
-    public required PriceLadder ToBack { get; init; }
+    public PriceLadder ToBack { get; init; }
 
     /// <summary>
     /// The traded volume by distinct price, if requested. See <see cref="MarketDataFilter"/>
     /// </summary>
-    public required PriceLadder Traded { get; init; }
+    public PriceLadder Traded { get; init; }
 
     /// <summary>
     /// Last price traded on this selection. Only returned if requested with <see cref="MarketDataFilter"/>
     /// </summary>
-    public required double LastTradedPrice { get; init; }
+    public double LastTradedPrice { get; init; }
 
     /// <summary>
     /// 
     /// </summary>
-    public required double StartingPriceNear { get; init; }
+    public double StartingPriceNear { get; init; }
 
     /// <summary>
     /// 
     /// </summary>
-    public required double StartingPriceFar { get; init; }
+    public double StartingPriceFar { get; init; }
 }
--- tests/BetfairDotNet.Tests/FactoryTests/MarketSnapshotFactoryTests.cs
+++ tests/BetfairDotNet.Tests/FactoryTests/MarketSnapshotFactoryTests.cs
@@ -1,5 +1,7 @@
-﻿using BetfairDotNet.Enums.Streaming;
+﻿using BetfairDotNet.Enums.Betting;
+using BetfairDotNet.Enums.Streaming;
 using BetfairDotNet.Factories;
+using BetfairDotNet.Models.Betting;
 using BetfairDotNet.Models.Streaming;
 using FluentAssertions;
 using System.Collections.Concurrent;
@@ -23,5 +25,82 @@ public class MarketSnapshotFactoryTests {
 
         // Assert
         actual.Should().BeEmpty();
+    }
+
+
+    [Fact]
+    public void ProcessImage_ShouldReturnMarketSnapshot_WhenMessageIsImage() {
+        // Arrange
+        var atl = new List<List<double>>() { new() { 1.23, 150 }, new() { 1.24, 300 } };
+        var atb = new List<List<double>>() { new() { 1.25, 150 }, new() { 1.26, 300 } };
+        var trd = new List<List<double>>() { new() { 1.27, 100 }, new() { 1.28, 100 } };
+        var rnrChange = new RunnerChange() { Id = 12345, LastTradedPrice = 1.29, AvailableToBack = atb, AvailableToLay = atl, TradedVolume = trd };
+        var rnrChanges = new List<RunnerChange> { rnrChange };
+        var rnrDef = new RunnerDefinition() { Id = 12345, SortPriority = 1, Status = RunnerStatusEnum.ACTIVE };
+        var rnrDefs = new List<RunnerDefinition> { rnrDef };
+        var marketDef = new MarketDefinition() { Status = MarketStatusEnum.OPEN, Runners = rnrDefs };
+        var marketChange = new MarketChange() { Id = "marketId", IsImage = true, RunnerChanges = rnrChanges, MarketDefinition = marketDef };
+        var marketChanges = new List<MarketChange> { marketChange };
+        var changeMessage = new MarketChangeMessage() { Id = 123, ChangeType = ChangeTypeEnum.SUB_IMAGE, MarketChanges = marketChanges };
+
+        var expAtl = new PriceLadder(SideEnum.LAY, new List<PriceSize> { new(1.23, 150), new(1.24, 300) });
+        var expAtb = new PriceLadder(SideEnum.BACK, new List<PriceSize> { new(1.25, 150), new(1.26, 300) });
+        var expTrd = new PriceLadder(SideEnum.BACK, new List<PriceSize> { new(1.27, 100), new(1.28, 100) });
+        var expRnrDef = new RunnerDefinition() { Id = 12345, SortPriority = 1, Status = RunnerStatusEnum.ACTIVE };
+        var expRnrSnap = new RunnerSnapshot() { SelectionId = 12345, LastTradedPrice = 1.29, ToBack = expAtb, ToLay = expAtl, Traded = expTrd, RunnerDefinition = expRnrDef };
+        var expRnrSnaps = new Dictionary<long, RunnerSnapshot> { [12345] = expRnrSnap };
+        var expMarketDef = new MarketDefinition() { Status = MarketStatusEnum.OPEN, Runners = new List<RunnerDefinition> { expRnrDef } };
+        var expectedSnapshot = new MarketSnapshot() { MarketId = "marketId", MarketDefinition = expMarketDef, RunnerSnapshots = expRnrSnaps };
+        var cache = new ConcurrentDictionary<string, MarketSnapshot>();
+
+        // Act
+        var sut = new MarketSnapshotFactory(cache);
+        var actual = sut.GetSnapshots(changeMessage).First();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expectedSnapshot);
+    }
+
+
+    [Fact]
+    public void ProcessImage_ShouldReturnMarketSnapshot_WhenMessageIsDelta() {
+        // Arrange
+        var atl = new List<List<double>>() { new() { 1.23, 150 } };
+        var atb = new List<List<double>>() { new() { 1.25, 75 }, new() { 1.26, 0 } };
+        var trd = new List<List<double>>() { new() { 1.28, 100 } };
+        var rnrChange = new RunnerChange() { Id = 12345, LastTradedPrice = 1.29, AvailableToBack = atb, AvailableToLay = atl, TradedVolume = trd };
+        var rnrChanges = new List<RunnerChange> { rnrChange };
+        var rnrDef = new RunnerDefinition() { Id = 12345, SortPriority = 1, Status = RunnerStatusEnum.ACTIVE };
+        var rnrDefs = new List<RunnerDefinition> { rnrDef };
+        var marketDef = new MarketDefinition() { Status = MarketStatusEnum.OPEN, Runners = rnrDefs };
+        var marketChange = new MarketChange() { Id = "marketId", IsImage = false, RunnerChanges = rnrChanges, MarketDefinition = marketDef };
+        var marketChanges = new List<MarketChange> { marketChange };
+        var changeMessage = new MarketChangeMessage() { Id = 123, ChangeType = ChangeTypeEnum.DELTA, MarketChanges = marketChanges };
+
+        var cachedAtl = new PriceLadder(SideEnum.LAY, new List<PriceSize> { new(1.24, 300) });
+        var cachedAtb = new PriceLadder(SideEnum.BACK, new List<PriceSize> { new(1.26, 300), new(1.25, 75) });
+        var cachedTrd = new PriceLadder(SideEnum.BACK, new List<PriceSize> { new(1.27, 100) });
+        var cachedRnrDef = new RunnerDefinition() { Id = 12345, SortPriority = 1, Status = RunnerStatusEnum.ACTIVE };
+        var cachedRnrSnap = new RunnerSnapshot() { SelectionId = 12345, LastTradedPrice = 1.28, ToBack = cachedAtb, ToLay = cachedAtl, Traded = cachedTrd, RunnerDefinition = cachedRnrDef };
+        var cachedRnrSnaps = new Dictionary<long, RunnerSnapshot> { [12345] = cachedRnrSnap };
+        var cachedMarketDef = new MarketDefinition() { Status = MarketStatusEnum.SUSPENDED, Runners = new List<RunnerDefinition> { cachedRnrDef } };
+        var cachedSnapshot = new MarketSnapshot() { MarketId = "marketId", MarketDefinition = cachedMarketDef, RunnerSnapshots = cachedRnrSnaps };
+        var cache = new ConcurrentDictionary<string, MarketSnapshot>() { ["marketId"] = cachedSnapshot };
+
+        var expAtl = new PriceLadder(SideEnum.LAY, new List<PriceSize> { new(1.23, 150), new(1.24, 300) });
+        var expAtb = new PriceLadder(SideEnum.BACK, new List<PriceSize> { new(1.25, 75) });
+        var expTrd = new PriceLadder(SideEnum.BACK, new List<PriceSize> { new(1.28, 100), new(1.27, 100) });
+        var expRnrDef = new RunnerDefinition() { Id = 12345, SortPriority = 1, Status = RunnerStatusEnum.ACTIVE };
+        var expRnrSnap = new RunnerSnapshot() { SelectionId = 12345, LastTradedPrice = 1.29, ToBack = expAtb, ToLay = expAtl, Traded = expTrd, RunnerDefinition = expRnrDef };
+        var expRnrSnaps = new Dictionary<long, RunnerSnapshot> { [12345] = expRnrSnap };
+        var expMarketDef = new MarketDefinition() { Status = MarketStatusEnum.OPEN, Runners = new List<RunnerDefinition> { expRnrDef } };
+        var expSnapshot = new MarketSnapshot() { MarketId = "marketId", MarketDefinition = expMarketDef, RunnerSnapshots = expRnrSnaps };
+
+        // Act
+        var sut = new MarketSnapshotFactory(cache);
+        var actual = sut.GetSnapshots(changeMessage).First();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expSnapshot);
     }
 }

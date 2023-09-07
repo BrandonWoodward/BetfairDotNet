3acb19719bf2d50d6e456c7822dc4df8c86867eb
Brandon <bkw1491@gmail.com>
2023-09-07 17:53:44 +0100

    Add missing test cases

---
+++ tests/BetfairDotNet.Tests/ModelsTests/Betting/UpdateInstructionReportTests.cs
@@ -0,0 +1,31 @@
+﻿using BetfairDotNet.Converters;
+using BetfairDotNet.Enums.Betting;
+using BetfairDotNet.Models.Betting;
+using FluentAssertions;
+using Xunit;
+
+namespace BetfairDotNet.Tests.ModelsTests.Betting;
+
+public class UpdateInstructionReportTests {
+
+
+    [Fact]
+    public void UpdateInstructionReport_ShouldSerializeProperly() {
+        // Arrange
+        var updateInstructionReport = new UpdateInstructionReport {
+            Status = InstructionReportStatusEnum.SUCCESS,
+            ErrorCode = InstructionReportErrorCodeEnum.INVALID_BET_SIZE,
+            Instruction = new UpdateInstruction {
+                BetId = "some-bet-id",
+                NewPersistenceType = PersistenceTypeEnum.LAPSE,
+            }
+        };
+
+        // Act
+        var json = JsonConvert.Serialize(updateInstructionReport);
+        var deserializedUpdateExecutionReport = JsonConvert.Deserialize<UpdateInstructionReport>(json);
+
+        // Assert
+        deserializedUpdateExecutionReport.Should().BeEquivalentTo(updateInstructionReport);
+    }
+}
---
+++ tests/BetfairDotNet.Tests/ModelsTests/Heartbeat/KeepAliveResponseTests.cs
@@ -0,0 +1,23 @@
+﻿using BetfairDotNet.Converters;
+using BetfairDotNet.Enums.Account;
+using BetfairDotNet.Models.Heartbeat;
+using FluentAssertions;
+using Xunit;
+
+namespace BetfairDotNet.Tests.ModelsTests.Heartbeat;
+
+public class KeepAliveResponseTests {
+
+    [Fact]
+    public void KeepAliveResponse_ShouldSerializeProperly() {
+        // Arrange
+        var keepAliveResponse = new KeepAliveResponse() { Status = LoginStatusEnum.SUCCESS };
+
+        // Act
+        var json = JsonConvert.Serialize(keepAliveResponse);
+        var deserialized = JsonConvert.Deserialize<KeepAliveResponse>(json);
+
+        // Assert
+        deserialized.Should().BeEquivalentTo(keepAliveResponse);
+    }
+}

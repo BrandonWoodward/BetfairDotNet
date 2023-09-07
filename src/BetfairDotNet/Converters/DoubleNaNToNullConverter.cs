4c870ab9dacbee26f79dee511136251391f2d345
Brandon <bkw1491@gmail.com>
2023-09-07 18:21:50 +0100

    Catch "infinity" edge case

--- src/BetfairDotNet/Converters/DoubleNaNToNullConverter.cs
+++ src/BetfairDotNet/Converters/DoubleNaNToNullConverter.cs
@@ -9,11 +9,13 @@ internal class DoubleNaNToNullConverter : JsonConverter<double?> {
     public override double? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
         if(reader.TokenType == JsonTokenType.String) {
             var stringValue = reader.GetString();
-            return stringValue == "NaN"
-                ? null
-                : double.TryParse(stringValue, out var value)
-                ? value
-                : throw new JsonException($"Unexpected value {stringValue} for double.");
+            if(stringValue == "NaN" || stringValue == "Infinity" || stringValue == "-Infinity") {
+                return null;
+            }
+            if(double.TryParse(stringValue, out var value)) {
+                return value;
+            }
+            throw new JsonException($"Unexpected value {stringValue} for double.");
         }
         else if(reader.TokenType == JsonTokenType.Number) {
             return reader.GetDouble();

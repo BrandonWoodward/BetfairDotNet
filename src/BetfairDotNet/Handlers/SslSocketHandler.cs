fcffd0f3a0233bc4202ccc679b137b17e32b0302
Brandon <bkw1491@gmail.com>
2023-09-15 18:13:50 +0100

    Change buffer size

--- src/BetfairDotNet/Handlers/SslSocketHandler.cs
+++ src/BetfairDotNet/Handlers/SslSocketHandler.cs
@@ -13,7 +13,8 @@ using System.Text;
 namespace BetfairDotNet.Handlers;
 
 
-internal sealed class SslSocketHandler : ISslSocketHandler {
+internal sealed class SslSocketHandler : ISslSocketHandler
+{
 
     private readonly Subject<ReadOnlyMemory<byte>> _messageSubject = new();
     private readonly Subject<Unit> _recoverySubject = new();
@@ -35,31 +36,36 @@ internal sealed class SslSocketHandler : ISslSocketHandler {
         => _recoverySubject.AsObservable();
 
 
-    internal SslSocketHandler(ISslSocket sslStream, string endpoint) {
+    internal SslSocketHandler(ISslSocket sslStream, string endpoint)
+    {
         _socket = sslStream;
         _endpoint = endpoint;
     }
 
 
-    public async Task Start(int recoveryThreshold, int maxRecoveryWait) {
+    public async Task Start(int recoveryThreshold, int maxRecoveryWait)
+    {
         _recoveryThreshold = TimeSpan.FromMilliseconds(recoveryThreshold);
         _cts = new CancellationTokenSource();
         _lastReceivedTime = DateTime.UtcNow;
 
-        try {
+        try
+        {
             await _socket.ConnectAsync(_endpoint, _port, maxRecoveryWait);
             await _socket.AuthenticateAsClientAsync(_endpoint);
             _listener = new Thread(() => ReceiveLines(_cts.Token)) { IsBackground = true };
             _listener.Start();
         }
-        catch(SocketException ex) {
+        catch(SocketException ex)
+        {
             var exception = new BetfairESAException("Failed to start stream. See InnerException", ex);
             _messageSubject.OnError(exception);
         }
     }
 
 
-    public void Stop() {
+    public void Stop()
+    {
         _cts?.Cancel();
         _cts?.Dispose();
         if(_socket.IsConnected()) _socket?.Close();
@@ -67,36 +73,44 @@ internal sealed class SslSocketHandler : ISslSocketHandler {
     }
 
 
-    public async Task SendLine<T>(T message) where T : BaseMessage {
+    public async Task SendLine<T>(T message) where T : BaseMessage
+    {
         message.Id = Interlocked.Increment(ref _incrementId);
         var messageJson = JsonConvert.Serialize(message);
         var messageBytes = Encoding.UTF8.GetBytes(messageJson + "\r\n"); // CRLF is required
 
-        try {
+        try
+        {
             await _socket.WriteAsync(messageBytes);
         }
-        catch(IOException ex) {
+        catch(IOException ex)
+        {
             HandleException("Failed to send line. See InnerException", ex);
         }
-        catch(SocketException ex) {
+        catch(SocketException ex)
+        {
             HandleException("Failed to send line. See InnerException", ex);
         }
     }
 
 
-    private void ReceiveLines(CancellationToken token) {
-        var buffer = ArrayPool<byte>.Shared.Rent(1024 * 256); // Unsure of optimal size
+    private void ReceiveLines(CancellationToken token)
+    {
+        var buffer = ArrayPool<byte>.Shared.Rent(2 * 1024 * 1024); // Unsure of optimal size
         var delimiter = "\r\n"u8.ToArray(); // Separate each message to processs individually
         var bufferOffset = 0;
 
-        try {
-            while(_socket.IsConnected() && !token.IsCancellationRequested) {
+        try
+        {
+            while(_socket.IsConnected() && !token.IsCancellationRequested)
+            {
                 _lastReceivedTime = DateTime.UtcNow;
 
                 var bytesRead = _socket.Read(buffer, bufferOffset, buffer.Length - bufferOffset);
 
                 // If we haven't received a message in the last heartbeat interval, then we have a problem
-                if(_recoveryThreshold.HasValue && DateTime.UtcNow - _lastReceivedTime > _recoveryThreshold.Value) {
+                if(_recoveryThreshold.HasValue && DateTime.UtcNow - _lastReceivedTime > _recoveryThreshold.Value)
+                {
                     _recoverySubject.OnNext(Unit.Default); // Initiate recovery
                     break;
                 }
@@ -104,23 +118,27 @@ internal sealed class SslSocketHandler : ISslSocketHandler {
                 bufferOffset += bytesRead;
                 int foundIndex;
 
-                while((foundIndex = buffer.AsSpan(0, bufferOffset).IndexOf(delimiter)) != -1) {
+                while((foundIndex = buffer.AsSpan(0, bufferOffset).IndexOf(delimiter)) != -1)
+                {
                     _messageSubject.OnNext(buffer.AsMemory(0, foundIndex));
                     bufferOffset -= foundIndex + delimiter.Length;
                     buffer.AsSpan().Slice(foundIndex + delimiter.Length, bufferOffset).CopyTo(buffer);
                 }
             }
         }
-        catch(SocketException ex) {
+        catch(SocketException ex)
+        {
             HandleException("Failed to receive line. See InnerException", ex);
         }
-        finally {
+        finally
+        {
             ArrayPool<byte>.Shared.Return(buffer); // Avoid memory leak
         }
     }
 
 
-    private void HandleException(string message, Exception ex) {
+    private void HandleException(string message, Exception ex)
+    {
         var exception = new BetfairESAException($"{message} See InnerException for details.", ex);
         _messageSubject.OnError(exception);
     }

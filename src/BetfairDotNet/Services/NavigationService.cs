ab0d7d38064ec3f87e1378a7cb0d3027f5c9b254
Brandon <bkw1491@gmail.com>
2023-10-01 10:07:00 +0100

    Add docstrings.

--- src/BetfairDotNet/Services/NavigationService.cs
+++ src/BetfairDotNet/Services/NavigationService.cs
@@ -4,6 +4,9 @@ using BetfairDotNet.Models.Navigation;
 
 namespace BetfairDotNet.Services;
 
+/// <summary>
+/// A service that provides functionality for replicating the Betfair navigation menu.
+/// </summary>
 public sealed class NavigationService
 {
     
@@ -13,7 +16,15 @@ public sealed class NavigationService
     {
         _requestResponseHandler = requestResponseHandler;
     }
-
+    
+    /// <summary>
+    /// This Navigation Data for Applications service allows the retrieval of the full Betfair market navigation menu
+    /// from a compressed file. 
+    /// </summary>
+    /// <remarks>
+    /// The file data is cached and new request for the file once an hour should be suitable
+    /// for those looking to accurately recreate the Betfair navigation menu.
+    /// </remarks>
     public Task<NavigationRoot> NavigationMenu()
     {
         return _requestResponseHandler.Request<NavigationRoot>(
